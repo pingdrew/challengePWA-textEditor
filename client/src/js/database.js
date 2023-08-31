de155ea02fc76d634e2b45df3f9a83c6f1688306
pingdrew <pingdrew0@gmail.com>
2023-08-31 17:15:10 -0500

    finished server side, working on client js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,42 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+
+  console.log('PUT to the database');
+  
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store
+  const store = tx.objectStore('jate');
+
+  //store and pass in the content.
+  const request = store.put({ value:content });
+
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store
+  const store = tx.objectStore('jate');
+
+  // get all data in the database.
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
 
 initdb();
